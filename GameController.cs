9b5bc1515e9ff1cf85e65bf7d31e88d0b609f698
alu0100904932 <alu0100904932@ull.edu.es>
2018-10-30 11:21:46 +0000

    Añadidos Scripts

---
+++ GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public delegate void PlayerEvents();
+    public static event PlayerEvents CambioLuz;
+    public static event PlayerEvents AumentoPoder;
+    public static event PlayerEvents ReduccionPoder;
+
+    void Update()
+    {   if (Input.GetKeyDown(KeyCode.L))
+        {
+            CambioLuz();
+        }            
+    }
+
+    public static void AumentarPoder()
+    {
+        if (AumentoPoder != null)
+        {
+            AumentoPoder();
+        }
+    }
+
+    public static void ReducirPoder()
+    {
+        if (ReduccionPoder != null)
+        {
+            ReduccionPoder();
+        }
+    }
+}
